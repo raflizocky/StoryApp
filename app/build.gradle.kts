260e75ffaa7393bb59b7e9cea03f71e8d2c4fb71
rafli zocky <raflizocky@gmail.com>
2024-11-16 14:10:02 +0700

    fix google maps not showing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "GOOGLE_MAPS_API_KEY",
+            "\"${project.findProperty("google_maps_api_key") ?: ""}\"")
     }
 
     buildTypes {
